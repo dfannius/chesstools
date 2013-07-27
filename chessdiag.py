cd6bf747f609031794bb0d531573ff7eb377ae62
Dan Schmidt <dfan@dfan.org>
2013-07-26 21:39:54 -0400

    diag2fen.py functional

--- chessdiag.py
+++ chessdiag.py
@@ -1,5 +1,8 @@
-from PIL import Image
+from PIL import Image, ImageChops
 import os
+
+def img_equal( im1, im2 ):
+    return ImageChops.difference( im1, im2 ).getbbox() is None
 
 REF_SQ_SIZE = 30                # size of squares in reference art
 REF_MARGIN = 15                 # width of margin in reference art
@@ -15,43 +18,6 @@ def image_has_margin( img ):
         elif width == 8 * REF_SQ_SIZE + 2 + REF_MARGIN:
             return True
     raise Exception( "Bad image dimensions %dx%d" % (width, height) )
-
-class Diagram:
-
-    def __init__( self, img ):
-        self.img = img
-        self.has_margin = image_has_margin( img )
-        self.margin = REF_MARGIN * self.has_margin
-
-    def coords_to_bounds( self, row, col ):
-        return ( (REF_SQ_SIZE * col + self.margin + 1,
-                  REF_SQ_SIZE * row + 1,
-                  REF_SQ_SIZE * (col + 1) + self.margin + 1,
-                  REF_SQ_SIZE * (row + 1) + 1) )
-
-    # Takes a square like 'a8' and turns it into a 4-tuple suitable for
-    # sending to PIL's Image.crop function
-    def sq_to_bounds( self, sq ):
-        row = ord( '8' ) - ord( sq[1] ) # 0 to 7 top to bottom
-        col = ord( sq[0] ) - ord( 'a' ) # 0 to 7 left to right
-        return self.coords_to_bounds( row, col )
-        
-    def coords_to_pic( self, row, col ):
-        return self.img.crop( self.coords_to_bounds( row, col ) )
-
-    def sq_to_pic( self, sq ):
-        return self.img.crop( self.sq_to_bounds( sq ) )
-
-# 0: W at bottom, W to move (plus all pieces as specified by square_dict)
-# 1: W at bottom, B to move
-# 2: B at bottom, W to move
-# 3: B at bottom, B to move
-ref_diagrams = [ Diagram( Image.open( "%s/reference%d.png" % (script_dir, i) ) ) for i in range( 1, 5 ) ]
-
-def get_margin_img( options ):
-    print "flip", options.flip, "black", options.black_to_move
-    print ref_diagrams
-    return ref_diagrams[2 * options.flip + options.black_to_move].img
 
 # Mapping of FEN character to dark/light squares in ref_diagrams[0]
 square_dict = {
@@ -69,7 +35,79 @@ square_dict = {
     'k': ('f4', 'f3'),
     ' ': ('g1', 'g2')
 }
-    
+
+# Declare this now so the Diagram constructor can refer to it
+ref_diagrams = []
+
+class Diagram:
+
+    def __init__( self, img, flipped=None, black_to_move=None ):
+        self.img = img
+        self.has_margin = image_has_margin( img )
+        self.margin = REF_MARGIN * self.has_margin
+        if self.has_margin:
+            if flipped is None:
+                bottom_margin = self.bottom_margin_pic()
+                for ref in ref_diagrams:
+                    if img_equal( bottom_margin, ref.bottom_margin_pic() ):
+                        self.flipped = ref.flipped
+                        self.black_to_move = ref.black_to_move
+            else:
+                self.flipped = flipped
+                self.black_to_move = black_to_move
+        else:
+            self.flipped = False
+            self.black_to_move = False
+
+    def left_margin_pic( self ):
+        MARGIN_END = 8 * REF_SQ_SIZE + REF_MARGIN + 1
+        return self.img.crop( (1, 1, REF_MARGIN + 1, MARGIN_END) )
+
+    def bottom_margin_pic( self ):
+        MARGIN_END = 8 * REF_SQ_SIZE + REF_MARGIN + 1
+        return self.img.crop( (1, MARGIN_END - REF_MARGIN, MARGIN_END, MARGIN_END) )
+
+    def coords_to_bounds( self, row, col ):
+        if self.flipped:
+            row = 7 - row
+            col = 7 - col
+        return ( (REF_SQ_SIZE * col + self.margin + 1,
+                  REF_SQ_SIZE * row + 1,
+                  REF_SQ_SIZE * (col + 1) + self.margin + 1,
+                  REF_SQ_SIZE * (row + 1) + 1) )
+
+    # Takes a square like 'a8' and turns it into a 4-tuple suitable for
+    # sending to PIL's Image.crop function
+    def sq_to_bounds( self, sq ):
+        row = ord( '8' ) - ord( sq[1] ) # 0 to 7 top to bottom
+        col = ord( sq[0] ) - ord( 'a' ) # 0 to 7 left to right
+        return self.coords_to_bounds( row, col )
+
+    def coords_to_pic( self, row, col ):
+        return self.img.crop( self.coords_to_bounds( row, col ) )
+
+    def sq_to_pic( self, sq ):
+        return self.img.crop( self.sq_to_bounds( sq ) )
+
+    def coords_to_char( self, row, col, ref_diag ):
+        pic = self.coords_to_pic( row, col )
+        for c in square_dict.keys():
+            for sq in square_dict[c]:
+                ref_pic = ref_diag.sq_to_pic( sq )
+                if img_equal( pic, ref_pic ):
+                    return c
+        return None
+
+# 0: W at bottom, W to move (plus all pieces as specified by square_dict)
+# 1: W at bottom, B to move
+# 2: B at bottom, W to move
+# 3: B at bottom, B to move
+ref_diagrams = [ Diagram( Image.open( "%s/reference%d.png" % (script_dir, i) ),
+                          i > 2, i % 2 == 0 ) for i in range( 1, 5 ) ]
+
+def get_margin_img( options ):
+    return ref_diagrams[2 * options.flip + options.black_to_move].img
+
 # 0 = dark, 1 = light
 def square_color( col, row ):
     return (col + row + 1) % 2
@@ -83,6 +121,7 @@ def add_square( img, col, row, piece, options ):
     img.paste( sq_pic, (REF_SQ_SIZE * display_col + 1 + margin,
                         REF_SQ_SIZE * display_row + 1) )
 
+# If the FEN says it's black to move, that overrides the command line
 def maybe_override_side( fen, options ):
     if options.black_to_move: return
     space_pos = fen.find( ' ' )
@@ -130,16 +169,13 @@ def add_margins( img, options ):
     img.paste( bottom_margin, bottom_margin_coords )
     return img
 
-def sq_to_char( img, row, col, has_margin ):
-    pic = sq_to_pic( img, row, col )
-
 def image_to_fen( img ):
+    diag = Diagram( img )
     fen_str = ""
-    has_margin = image_has_margin( img )
     num_spaces = 0
     for row in range( 8 ):
         for col in range( 8 ):
-            c = sq_to_char( img, row, col, has_margin )
+            c = diag.coords_to_char( row, col, ref_diagrams[0] )
             if c == " ":
                 num_spaces += 1
             else:
@@ -150,8 +186,11 @@ def image_to_fen( img ):
         if num_spaces > 0:
             fen_str += str( num_spaces )
             num_spaces = 0
-        if col < 7:
+        if row < 7:
             fen_str += "/"
     if num_spaces > 0:
         fen_str += str( num_spaces )
     fen_str += " "
+    fen_str += ("b" if diag.black_to_move else "w")
+    fen_str += " KQkq - 0 1"    # I don't really care about this
+    return fen_str
---
+++ diag2fen.py
@@ -0,0 +1,14 @@
+import argparse
+import chessdiag
+from PIL import Image
+
+parser = argparse.ArgumentParser( description="Read a file with a chess diagram and output a FEN string that represents it." )
+parser.add_argument( "input_name",
+                     help="Name of diagram file to read from" )
+global_options = parser.parse_args()
+
+img = Image.open( global_options.input_name )
+fen_str = chessdiag.image_to_fen( img )
+print fen_str
+
+
