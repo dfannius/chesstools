2f12c27bbe9e6891cc3a19ca669931f05f73fbe9
Dan Schmidt <dfan@dfan.org>
2013-11-25 13:23:54 -0500

    Also draw actual rating

--- perf.py
+++ perf.py
@@ -1,3 +1,10 @@
+# TODO
+# + Graph USCF rating as well
+# - Cache rating information
+# - Print date marks on x axis
+# - Refactor TournamentResultsParser and YearResultsParser
+# - cur_tag should really be a stack
+
 import matplotlib.pyplot as plt
 import numpy as np
 import re
@@ -50,6 +57,13 @@ def accurate_perf_rating( results ):
 def xtbl_to_str( xtbl ):
     return "%s-%s-%s" % (xtbl[0:4], xtbl[4:6], xtbl[6:8] )
 
+class TournamentResult():
+    def __init__( self, xtbl, rating ):
+        self.xtbl = xtbl
+        self.rating = rating
+    def __repr__( self ):
+        return "%s %d" % (self.xtbl, self.rating)
+
 class Result():
     def __init__( self, opp_rating, result, xtbl, rd ):
         self.opp_rating = opp_rating
@@ -66,15 +80,61 @@ def alist_find( alist, key ):
         if k == key: return v
     return None
 
-READ_RATING = 0
-READ_RESULT = 1
-rating_re = re.compile( r"=> (\d+)" )
-xtbl_re = re.compile( r"http://msa.uschess.org/XtblMain.php\?(\d+)" )
-class ResultsParser( HTMLParser ):
-    def __init__( self, results ):
-        self.state = READ_RATING
+tournament_rating_re = re.compile( r"=>" )
+rating_re = re.compile( r"=>\s+(\d+)", re.MULTILINE )
+xtbl_re = re.compile( r"XtblMain.php\?([\d]+)" )
+tnmt_hst_re = re.compile( r"MbrDtlTnmtHst.php\?[\d]+\.[\d]+" )
+
+class TournamentResultsParser( HTMLParser ):
+    def __init__( self ):
+        self.reading_data = False
+        self.tag_stack = []
+        self.cur_col = 0
+        self.in_td = 0
+        self.xtbl = None
         self.rating = 0
-        self.results = results
+        self.results = []
+        HTMLParser.__init__( self )
+
+    def handle_starttag( self, tag, attrs ):
+        self.tag_stack += tag
+        if tag == "tr":
+            self.cur_col = -1
+        if tag == "td":
+            self.cur_col += 1
+            self.in_td += 1
+        if tag == "a":
+            if self.cur_col == 1:
+                xtbl_link = alist_find( attrs, "href" )
+                m = xtbl_re.search( xtbl_link )
+                if m:
+                    self.xtbl = m.group( 1 )
+
+    def handle_endtag( self, tag ):
+        self.tag_stack = self.tag_stack[:-1]
+        if tag == "td":
+            self.in_td -= 1
+        if tag == "tr" and self.xtbl and self.rating != 0:
+            self.results.append( TournamentResult( self.xtbl, self.rating ) )
+            self.xtbl = None
+            self.rating = 0
+
+    def cur_tag( self ):
+        if len( self.tag_stack ) == 0:
+            return None
+        else:
+            return self.tag_stack[-1]
+
+    def handle_data( self, data ):
+        if self.in_td > 0:
+            if self.cur_col == 2 and self.cur_tag() == "b" and self.xtbl:
+                m = re.match( "\d+", data )
+                self.rating = int( m.group( 0 ) )
+
+class YearResultsParser( HTMLParser ):
+    def __init__( self ):
+        self.rating = 0
+        self.results = []
         self.indent = 0
         self.reading_data = False
         self.cur_tag = None
@@ -134,9 +194,12 @@ class ResultsParser( HTMLParser ):
 def year_stats_page_url( id, year ):
     return "http://main.uschess.org/datapage/gamestats.php?memid=%s&ptype=Y&rs=R&dkey=%d&drill=Y" % (id, year )
 
+def tournament_stats_page_url( id ):
+    return "http://main.uschess.org/assets/msa_joomla/MbrDtlTnmtHst.php?%s" % id
+
 def year_stats( id, year ):
     u = urllib2.urlopen( year_stats_page_url( id, year ) )
-    parser = ResultsParser( [] )
+    parser = YearResultsParser()
     parser.feed( u.read() )
     return parser.results
 
@@ -166,28 +229,67 @@ def run_by_year( id ):
     for y in range( 1994, 1995 ):
         parse_year_stats( id, y )
 
+def xtbls_to_ratings( xtbls ):
+    mapping = {}
+    for (i, x) in enumerate( xtbls ):
+        mapping[x] = i          # overwrite earlier indices with later ones
+    vals = []
+    for (k, v) in mapping.items():
+        vals.append( (v, k ) )
+    return sorted( vals )
+
 def run_by_window( id ):
+    print "Getting tournament history..."
+    tnmt_results = get_tournament_history( id )
+    tnmt_map = {}
+    for r in tnmt_results:
+        tnmt_map[r.xtbl] = r.rating
     results = []
-    window_size = 20
+    window_size = 40
+    print "Getting yearly stats..."
     for y in range( 1994, 2014 ):
         results.extend( year_stats( id, y ) )
     results.sort( key=attrgetter( "xtbl", "rd" ) )
 #   print "      Date  Fast  Acc Result"
     ratings = []
-    for x in range( 20, len( results ) ):
+    tnmt_ratings = []
+    xtbls = []
+    print "Generating graph..."
+    for x in range( window_size, len( results ) ):
         begin = max( 0, x - window_size )
         r = results[begin:x]
         naive_perf = sum( r.val() for r in r ) / len( r )
         accurate_perf = accurate_perf_rating( r )
         ratings.append( accurate_perf )
+        xtbls.append( r[-1].xtbl )
 #        print "%s: %4d %4d %s %d" % (xtbl_to_str( r[-1].xtbl ),
 #                                     round( naive_perf ),
 #                                     round( accurate_perf ),
 #                                     r[-1].result,
 #                                     r[-1].opp_rating)
-    plt.plot( ratings )
+    tnmt_ratings = xtbls_to_ratings( xtbls )
+    tnmt_indices = [i for (i,x) in tnmt_ratings]
+    tnmt_ratings = [tnmt_map[x] for (i,x) in tnmt_ratings]
+    plt.plot( range( len( ratings ) ), ratings )
+    plt.plot( tnmt_indices, tnmt_ratings )
     plt.savefig( "%s.png" % id )
+    print "Done."
 
+def get_tournament_history( id ):
+    tnmt_u = urllib2.urlopen( tournament_stats_page_url( id ) )
+    tnmt_pages = []
+    results = []
+    for l in tnmt_u:
+        m = tnmt_hst_re.search( l )
+        if m:
+            tnmt_pages.append( m.group( 0 ) )
+    for page in tnmt_pages:
+        u = urllib2.urlopen( "http://main.uschess.org/assets/msa_joomla/" + page )
+        parser = TournamentResultsParser()
+        parser.feed( u.read() )
+        results.extend( parser.results )
+    return results
+    
 def run():
     run_by_window( sys.argv[1] )
 
